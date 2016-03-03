8e0d9edfe2e32cc5476f3594039dd647c54e3131
Abhisek- <pnabhisek@gmail.com>
2016-03-03 21:33:42 +0530

    predict file added

--- mrs/recsys/predict-dev.py
+++ mrs/recsys/predict-dev.py
@@ -169,13 +169,91 @@ class Predict:
         for i in range(len(ratings_for_test)):
             print(NN.feedforward(test_feature[i]), test_y[i])
 
+    @staticmethod
+    def f(rating):
+        """
+        converts the rating to a format for a five output neuron
+        
+        :param rating: its a rating value given to a movie item
+        :type rating:  int
+        """
+        l = [0, 0, 0, 0, 0]
+        l[rating - 1] = 1
+        return l
 
-    def create_training_example_with_item(self):
+    def f_inverse(l):
+        """
+        inverse function of f
+        
+        :param l: list of 0s and 1s
+        :type l:  list
+        """
+        return l.index(1) + 1
+
+    def create_training_examples_with_item(self, ratings):
+        """
+        :param ratings: list of tuples with item_id at 0th index and rating at 1th index
+        :type ratings:  list
+        :return: list of tuples of training examples and each training example contains item feature and rating given to that item
+        :rtype:  list
+        """
+        feature = []
+        for item_id, rating in ratings:
+            feature.append((list(self.data.get_item_by_id(item_id).get_genres().values()), self.f(rating)))
+
+        return feature
+
+    def create_training_examples_with_item_and_user_rating(self):
         """
         """
         pass
 
-    def create_training_example_with_item_and_user_rating(self):
+    def training_and_test_for_an_user_with_item(self, user_id):
         """
+        This method roughly does the following:
+
+        1. find how many movies user_name_of(user_id) has rated
+        2. divide the number of ratings to 80% (for training) and 20% (for test)
+        3. create ndarray both form 80% and 20% of the information separately
+        4. train it
+        5. test it
+
+        :param user_id: the id of the user for which you wanted to train and test
+        :type user_id:  int
+        """
+        ratings_by_user_id = list(self.data.get_user_by_id(user_id).get_movie_rating().items())
+        
+        # find how many movies user_name_of(user_id) has rated
+        nratings_by_user_id = len(ratings_by_user_id)
+
+        # divide the number of ratings to 80% (for training) and 20% (for test)
+        nratings_for_train  = int(nratings_by_user_id * .8)
+        nratings_for_test   = nratings_by_user_id - nratings_for_train
+
+        ratings_for_train = ratings_by_user_id[:nratings_for_train]
+        ratings_for_test  = ratings_by_user_id[nratings_for_train:]
+
+        # create ndarray both from 80% and 20% of the information separately
+        train = self.create_training_examples_with_item(ratings_for_train)
+        test  = self.create_training_examples_with_item(ratings_for_test)
+
+        # train it
+        NN = ann.Neural_Network()
+
+        # test it
+
+    def training_and_test_for_an_user_with_item_and_user_rating(self, user_id):
+        """
+        This method roughly does the following:
+
+        1. sort neighbors according to the similarity
+        2. find how many movies user_name_of(user_id) has rated
+        3. divide the number of ratings to 80% (for training) and 20% (for test)
+        4. create ndarray both form 80% and 20% of the information separately
+        5. train it
+        6. test it
+
+        :param user_id: the id of the user for which you wanted to train and test
+        :type user_id:  int
         """
         pass
