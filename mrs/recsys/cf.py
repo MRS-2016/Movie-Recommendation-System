f5f1dbf08262e83d9f1933a88136840edac448e7
Abhisek- <pnabhisek@gmail.com>
2016-03-04 14:20:00 +0530

    Correlation class added

--- mrs/recsys/cf.py
+++ mrs/recsys/cf.py
@@ -2,3 +2,12 @@
 # -*- coding: utf-8 -*-
 
 # create a class for correlation matrix and other class to find out similar users who have rated a particular movie w.r.t. a particular user based on correlation
+
+import numpy as np
+import pandas as pd
+
+class Correlation:
+    """
+    """
+    def pearson(self, rating_matrix):
+        return pd.DataFrame(rating_matrix.T).corr().as_matrix()
