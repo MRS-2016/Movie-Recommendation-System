51ca142f669b048d4cbf0f98920306326034b840
Abhisek- <pnabhisek@gmail.com>
2016-03-04 10:58:12 +0530

    f_inverse and f_inverse_cap moved to convert.py

---
+++ mrs/recsys/convert.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def f_inverse_cap(l):
+    no, val = 1, l[0]
+    for i, x in enumerate(l):
+        if x > val:
+            no = i + 1
+            val = x
+            return no
+
+def f_inverse(l):
+    return l.index(1) + 1
