583409988e74663da13822ba01f1979c7d23d798
Abhisek- <pnabhisek@gmail.com>
2016-03-07 12:27:41 +0530

    create_rating_matrix_with_missing_as_zero method added

--- mrs/datamodel/matrix.py
+++ mrs/datamodel/matrix.py
@@ -9,7 +9,7 @@ class Matrix:
     """
     """
     def __init__(self):
-        self.rating_matrix = np.full(([nusers + 1, nitems + 1]), fill_value = np.nan)
+        self.rating_matrix = None
 
     def create_rating_matrix(self, users):
         """
@@ -18,6 +18,20 @@ class Matrix:
         :param users: contains user ids as keys and instance of :py:mod:`mrs.datamodel.user.User` as values
         :type users:  dict
         """
+        self.rating_matrix = np.full(([nusers + 1, nitems + 1]), fill_value = np.nan)
+        
+        for user_id, user_profile in users.items():
+            for movie, rating in user_profile.get_movie_rating().items():
+                self.rating_matrix[user_id, movie] = rating[0]
+
+
+    def create_rating_matrix_with_missing_as_zero(self, users):
+        """
+        Same as :py:meth:`create_rating_matrix` except the missing values are zeros instead of nan
+        """
+        
+        self.rating_matrix = np.zeros(([nusers + 1, nitems + 1]))
+
         for user_id, user_profile in users.items():
             for movie, rating in user_profile.get_movie_rating().items():
                 self.rating_matrix[user_id, movie] = rating[0]
